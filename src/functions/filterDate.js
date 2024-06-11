f29b2a24099f13a18b6ab43e2295cae83b40e128
Glya-Corporation <alfonsouzcategui1811@gmail.com>
2024-06-11 11:58:47 -0500

    update filterDate.js

--- src/functions/filterDate.js
+++ src/functions/filterDate.js
@@ -9,6 +9,14 @@ export default filterDate = (dates, today) => {
       array.push({ date, ...rest });
     }
   }
+  
+  array.sort((a, b) => {
+    const [dayA, monthA, yearA] = a.date.split('-').map(Number);
+    const [dayB, monthB, yearB] = b.date.split('-').map(Number);
 
+    if (yearA !== yearB) return yearA - yearB;
+    if (monthA !== monthB) return monthA - monthB;
+    return dayA - dayB;
+  });
   return array;
 };
